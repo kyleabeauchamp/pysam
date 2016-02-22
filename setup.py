a67e83f2f0381d0ec2e269e54c1731d4c1e69a2c
Kyle Beauchamp <kyleabeauchamp@gmail.com>
2016-02-21 17:22:52 -0800

    Fix

--- setup.py
+++ setup.py
@@ -97,18 +97,17 @@ HTSLIB_CONFIGURE_OPTIONS = os.environ.get("HTSLIB_CONFIGURE_OPTIONS", None)
 # If cython is available, the pysam will be built using cython from
 # the .pyx files. If no cython is available, the C-files included in the
 # distribution will be used.
-if HTSLIB_MODE != "external":
-    try:
-        from cy_build import CyExtension as Extension, cy_build_ext as build_ext
-        source_pattern = "pysam/c%s.pyx"
-        cmdclass = {'build_ext': build_ext}
-        HTSLIB_MODE = "shared"
-    except ImportError:
-        # no Cython available - use existing C code
-        cmdclass = {}
-        source_pattern = "pysam/c%s.c"
-        # Set mode to separate, as "shared" not fully tested yet.
-        HTSLIB_MODE = "separate"
+try:
+    from cy_build import CyExtension as Extension, cy_build_ext as build_ext
+    source_pattern = "pysam/c%s.pyx"
+    cmdclass = {'build_ext': build_ext}
+    #HTSLIB_MODE = "shared"
+except ImportError:
+    # no Cython available - use existing C code
+    cmdclass = {}
+    source_pattern = "pysam/c%s.c"
+    # Set mode to separate, as "shared" not fully tested yet.
+    HTSLIB_MODE = "separate"
 
 # collect pysam version
 sys.path.insert(0, "pysam")
