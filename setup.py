51046e6bc48251f18345a1cb40b2bd65272a6db1
Kyle Beauchamp <kyleabeauchamp@gmail.com>
2016-02-21 17:20:02 -0800

    workaround

--- setup.py
+++ setup.py
@@ -87,7 +87,7 @@ def configure_library(library_dir, env_options=None, options=[]):
 #         pysam.
 # external: use shared libhts.so compiled outside of
 #           pysam
-HTSLIB_MODE = "external"
+HTSLIB_MODE = os.environ.get("HTSLIB_MODE", "external")
 HTSLIB_LIBRARY_DIR = os.environ.get("HTSLIB_LIBRARY_DIR", None)
 HTSLIB_INCLUDE_DIR = os.environ.get("HTSLIB_INCLUDE_DIR", None)
 HTSLIB_CONFIGURE_OPTIONS = os.environ.get("HTSLIB_CONFIGURE_OPTIONS", None)
@@ -97,17 +97,18 @@ HTSLIB_CONFIGURE_OPTIONS = os.environ.get("HTSLIB_CONFIGURE_OPTIONS", None)
 # If cython is available, the pysam will be built using cython from
 # the .pyx files. If no cython is available, the C-files included in the
 # distribution will be used.
-try:
-    from cy_build import CyExtension as Extension, cy_build_ext as build_ext
-    source_pattern = "pysam/c%s.pyx"
-    cmdclass = {'build_ext': build_ext}
-    HTSLIB_MODE = "shared"
-except ImportError:
-    # no Cython available - use existing C code
-    cmdclass = {}
-    source_pattern = "pysam/c%s.c"
-    # Set mode to separate, as "shared" not fully tested yet.
-    HTSLIB_MODE = "separate"
+if HTSLIB_MODE != "external":
+    try:
+        from cy_build import CyExtension as Extension, cy_build_ext as build_ext
+        source_pattern = "pysam/c%s.pyx"
+        cmdclass = {'build_ext': build_ext}
+        HTSLIB_MODE = "shared"
+    except ImportError:
+        # no Cython available - use existing C code
+        cmdclass = {}
+        source_pattern = "pysam/c%s.c"
+        # Set mode to separate, as "shared" not fully tested yet.
+        HTSLIB_MODE = "separate"
 
 # collect pysam version
 sys.path.insert(0, "pysam")
